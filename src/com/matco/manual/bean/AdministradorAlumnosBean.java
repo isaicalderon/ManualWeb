e9db6c02c99328339d1098e4ffc632925b600377
isaicalderon <isai.acalderon@gmail.com>
2019-03-05 23:56:22 -0700

    v1.0.0

---
+++ src/com/matco/manual/bean/AdministradorAlumnosBean.java
@@ -0,0 +1,18 @@
+package com.matco.manual.bean;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name = "administradorAlumnosBean")
+@ViewScoped
+
+public class AdministradorAlumnosBean extends GenericBean implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7062286680165571605L;
+
+}
